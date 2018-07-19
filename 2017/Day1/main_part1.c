dc2d52a8e59aad77ad02aefde66feb562d3eb382
António Sousa <antonio.sousa@fraunhofer.pt>
2018-07-19 17:11:29 +0100

    Day1 now using getline

--- 2017/Day1/main_part1.c
+++ 2017/Day1/main_part1.c
@@ -6,18 +6,9 @@ uint32_t read_input(char **data){
     if( *data != NULL ){
         return 0;
     }
-    *data = (char *)malloc(0);
 
-    char c = '0';
-    uint32_t n_read = 0;
-
-    while( (c = fgetc(stdin)) != '\n' ){
-        n_read++;
-        *data = (char*)realloc(*data, n_read * sizeof(char) );
-        (*data)[n_read-1] = c;
-    }
-
-    return n_read;
+    size_t s = 0;
+    return getline(data, &s, stdin)-1;
 }
 
 uint32_t get_next_index(uint32_t curr_index, uint32_t arr_size){
@@ -25,12 +16,10 @@ uint32_t get_next_index(uint32_t curr_index, uint32_t arr_size){
 }
 
 uint32_t get_sum(char *data, uint32_t size){
-    uint32_t next_index, sum = 0;
+    uint32_t sum = 0;
 
     for (uint32_t i = 0; i < size; i++) {
-        next_index = get_next_index(i, size);
-
-        if (data[i] == data[next_index]) {
+        if (data[i] == data[ get_next_index(i, size) ]) {
             sum += data[i] - '0';
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# AdventOfCode
+# Advent Of Code
+
+This repository contains my solutions to some (and eventually all) of the [Advent of Code](http://adventofcode.com/) challenges.
+
+Feel free to look around and leave some sugestions!
